b81f2d9964e0ed14c93c93583a35472eb3c050a2
liam-morrissey <liam.morrissey.family@gmail.com>
2020-02-21 16:19:10 -0700

    add test cases

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -4,11 +4,15 @@ import java.util.Map;
 import java.util.Set;
 
 public class IntBoard {
-	private int[][] boardCells;
+	private BoardCell[][] board;
 	private Map<BoardCell, Set<BoardCell>> adjacencies;
+	private Set<BoardCell> visited;
+	private Set<BoardCell> targets;
 	
 	public IntBoard() {
-		
+		super();
+		calcAdjacencies();
+		adjacencies = new HashMap<BoardCell, Set<BoardCell>>();
 	}
 	
 	public void calcAdjacencies() {
