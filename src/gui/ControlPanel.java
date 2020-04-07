2ce4e8f90f264609b31a31922c79d1f753e4b09e
BrandtRoss <bross@mymail.mines.edu>
2020-04-07 11:25:43 -0600

    Compacted control panel to single class

--- src/gui/ControlPanel.java
+++ src/gui/ControlPanel.java
@@ -6,6 +6,8 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 public class ControlPanel extends JPanel {
 	private JButton nextPlayer, makeAccusation;
@@ -30,5 +32,63 @@ public class ControlPanel extends JPanel {
 		add(dPanel);
 		add(gPanel);
 		add(grPanel);
+	}
+	
+	private class diePanel extends JPanel {
+		private JLabel rollLabel;
+		private JTextField roll;
+		
+		public diePanel() {
+			rollLabel = new JLabel("Roll");
+			roll = new JTextField(5);
+			roll.setEditable(false);
+			add(rollLabel);
+			add(roll);
+			
+			setBorder(new TitledBorder(new EtchedBorder(), "Die"));
+		}
+	}
+	
+	private class guessPanel extends JPanel{
+		private JLabel guessLabel;
+		private JTextField guess;
+		
+		public guessPanel() {
+			guessLabel = new JLabel("Guess Made");
+			guess = new JTextField(10);
+			guess.setEditable(false);
+			add(guessLabel);
+			add(guess);
+			
+			setBorder(new TitledBorder(new EtchedBorder(), "Guess"));
+		}
+	}
+	
+	private class guessResultPanel extends JPanel {
+		private JLabel responseLabel;
+		private JTextField response;
+		
+		public guessResultPanel() {
+			responseLabel = new JLabel("Response");
+			response = new JTextField(10);
+			response.setEditable(false);
+			add(responseLabel);
+			add(response);
+			
+			setBorder(new TitledBorder(new EtchedBorder(), "Guess Result"));
+		}
+	}
+	
+	private class turnPanel extends JPanel {
+		private JLabel turnLabel;
+		private JTextField turn;
+		
+		public turnPanel() {
+			turnLabel = new JLabel("Whose turn?");
+			turn = new JTextField(10);
+			turn.setEditable(false);
+			add(turnLabel);
+			add(turn);
+		}
 	}
 }
