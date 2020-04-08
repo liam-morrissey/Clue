cbdc59e2d95f30ded98ea70e21d957d5083b8d6f
BrandtRoss <bross@mymail.mines.edu>
2020-04-08 12:20:46 -0600

    Created file drop down menu

--- src/gui/ClueGUI.java
+++ src/gui/ClueGUI.java
@@ -2,8 +2,13 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class ClueGUI extends JFrame {
 	public ClueGUI() {
@@ -13,11 +18,38 @@ public class ClueGUI extends JFrame {
 		
 		ControlPanel cPanel = new ControlPanel();
 		add(cPanel, BorderLayout.SOUTH);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		menuBar.add(createFileMenu());
+	}
+	
+	/**
+	 * Creates the file drop down in the menu bar
+	 */
+	private JMenu createFileMenu() {
+		JMenu menu = new JMenu("File");
+		menu.add(displayDetective());
+		menu.add(createFileExitItem());
+		return menu;
+	}
+	
+	/**
+	 * Create the exit option that will exit the program when clicked
+	 */
+	private JMenuItem createFileExitItem() {
+		JMenuItem item = new JMenuItem("Exit");
+		item.addActionListener(e -> System.exit(0));
+		return item;
+	}
+	
+	private JMenuItem displayDetective() {
+		JMenuItem dispDetect = new JMenuItem("Display Detective Notes");
+		return dispDetect;
 	}
 	
 	public static void main(String[] args) {
 		ClueGUI gui = new ClueGUI();
 		gui.setVisible(true);
 	}
-
 }
